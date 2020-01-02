53eb9ace2704fd342ce4dcba868d51d537fce990
lecuong321 <lecuong09ck1@gmail.com>
2020-01-02 17:03:13 +0700

    change URI for heroku

--- src/admin/constants.js
+++ src/admin/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    API_URI: 'http://localhost:3000',
+    API_URI: 'https://smarthome2camera.herokuapp.com',
     DEMO_USER_ID: '9c7ff708-37bb-4993-84d7-e7538a00adb0',
     DEMO_USER_TOKEN: '036b15c7-dda9-4403-af10-cd509344a21c'
 });
