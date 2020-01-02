f93075696b499ca63fb50bc83bd27bf61302424e
lecuong321 <lecuong09ck1@gmail.com>
2020-01-02 17:11:14 +0700

    change URI

--- src/web/constants.js
+++ src/web/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    API_URI: 'https://smarthome2camera.herokuapp.com',
+    API_URI: 'https://smarthomelvbk.herokuapp.com',
     DEMO_USER_ID: '9c7ff708-37bb-4993-84d7-e7538a00adb0',
     DEMO_USER_TOKEN: 'b86af00b-c303-4b6b-ab42-15b4a14db502'
 });
