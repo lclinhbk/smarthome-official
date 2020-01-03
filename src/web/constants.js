d147c1b3eadfb1b274fb444505cb5fcd89858376
lecuong321 <lecuong09ck1@gmail.com>
2020-01-03 13:12:17 +0700

    change perfomance

--- src/web/constants.js
+++ src/web/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    API_URI: 'https://smarthomelvbk.herokuapp.com',
+    API_URI: 'http://localhost:3000',
     DEMO_USER_ID: '9c7ff708-37bb-4993-84d7-e7538a00adb0',
     DEMO_USER_TOKEN: 'b86af00b-c303-4b6b-ab42-15b4a14db502'
 });
